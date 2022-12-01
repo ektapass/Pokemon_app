b27e60981b8d225886e4e5e03823985ae892d18d
Ekta Bharti <ektabharti019@gmail.com>
2022-12-01 18:47:37 -0500

    commit

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 const  pokemon = require('./models/pokemon.js')
+app.set('view engine', 'jsx');
+  app.engine('jsx', require('express-react-views').createEngine());
+  app.get('/pokemon', function(req, res) {
+    res.render('Index',{allPokemon:pokemon});
+  });
+  app.get('/pokemon/:indexOfpokemon', (req, res) => {
+    //res.send(fruits[req.params.indexOfFruitsArray]);
+    res.render('Show', { //second param must be an object
+        pokemon:pokemon[req.params.indexOfpokemonArray] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+});
+  })
+  
 //module.exports = pokemon;
 app.listen(port,() => {
     console.log('listening on port' , port);
@@ -10,7 +22,19 @@ app.get('/', function(req, res) {
     res.send('<h1>Welcome to the Pokemon App!</h1>');
   });
   
-  app.get('/pokemon', function(req, res) {
-    res.send(pokemon);
-  });
+  
+//   app.set('view engine','jsx')
+// app.engine('jsx',require('express-react-views').createEngine())
+// app.get('/fruits/',(req,res)=>{
+//     res.render('Index',{fruits:fruits})
+// })
+// app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+//     //res.send(fruits[req.params.indexOfFruitsArray]);
+//     res.render('Show', { //second param must be an object
+//         fruit: fruits[req.params.indexOfFruitsArray] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+// });
+// })
+  
+  
+  
   
