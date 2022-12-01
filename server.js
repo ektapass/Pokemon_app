be9b2f4036b8c4eb792dc575e9110dbea78bf635
Ekta Bharti <ektabharti019@gmail.com>
2022-12-01 15:12:31 -0500

    third commit

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; 
+const  pokemon = require('./models/pokemon.js')
+//module.exports = pokemon;
 app.listen(port,() => {
     console.log('listening on port' , port);
 });
 app.get('/', function(req, res) {
     res.send('<h1>Welcome to the Pokemon App!</h1>');
   });
+  
+  app.get('/pokemon', function(req, res) {
+    res.send(pokemon);
+  });
+  
