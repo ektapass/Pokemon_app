c1295fb2179035c91b15312d2385146637e8ddf3
Ekta Bharti <ektabharti019@gmail.com>
2022-12-01 22:18:59 -0500

    all pokemon lists

--- views/index.jsx
+++ views/index.jsx
@@ -1,4 +1,5 @@
-const React = require('react')
+const React = require('react');
+const allPokemon = require('../models/pokemon');
 const myStyle = {
     color: '#ffffff',
     backgroundColor: '#000000',
@@ -6,8 +7,24 @@ const myStyle = {
 
   class Index extends React.Component {
     render(){
+      const pokemon = this.props
     return (
-      <div style={myStyle}>See All The Pokemon!</div>)
+      <div style={myStyle}>See All The Pokemon!
+        <ul>
+        {allPokemon.map((pokemon,i)=>{
+          return(
+     <li>
+      <a href={`/pokemon/${i}`}>{pokemon.name.charAt(0).toUpperCase()+ pokemon.name.slice(1)}</a>
+      
+ </li>
+          );
+        })}
+       </ul>
+      
+      
+      </div>
+      
+      )
     }
 }
   
